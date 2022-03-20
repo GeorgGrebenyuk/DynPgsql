f7bef4ebfc7f0c2e64fd2a42659a583a867aa823
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-20 17:17:26 +0300

    Create Connect.cs
    Dyn class to get connection

---
+++ src/DynPgsql/Connect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynPgsql.Core;
+
+namespace DynPgsql
+{
+	public class Connect
+	{
+		public static string SetConnectSettings(string host_name, string Username, string Password, string db_name)
+		{
+			return $"Host={host_name};Username={Username};Password={Password};Host={host_name};Database={db_name}";
+		}
+		public static void RunCommand(string conn_settings, string command)
+        {
+			ManageConnection conn = new ManageConnection(conn_settings);
+			conn.RunCommand(command);
+        }
+		
+	}
+}
