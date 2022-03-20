9f41faaae38a5d687c5259d096980c569ec179fa
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-20 17:17:07 +0300

    Create ManageConnection.cs
    Base class for create connection to db

---
+++ src/DynPgsql.Core/ManageConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Npgsql;
+
+namespace DynPgsql.Core
+{
+	public class ManageConnection
+	{
+		public NpgsqlConnection connetion;
+		public NpgsqlCommand command;
+
+		public ManageConnection(string connect_settings)
+		{
+			this.connetion = new NpgsqlConnection(connect_settings);
+			this.connetion.Open();
+			this.command = new NpgsqlCommand();
+			this.command.Connection = this.connetion;
+		}
+		public void Close()
+		{
+			this.command.Dispose();
+			if (this.connetion.State != System.Data.ConnectionState.Closed) this.connetion.Close();
+		}
+		public void RunCommand(string command, bool ExecuteWithoutReading = true)
+		{
+			this.command.CommandText = command;
+			if (ExecuteWithoutReading == true) this.command.ExecuteNonQuery();
+		}
+	}
+}
