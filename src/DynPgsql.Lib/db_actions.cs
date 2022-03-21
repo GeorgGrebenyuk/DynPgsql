b1287f55820c34c1f8199e5f92c582bf022f15ba
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-21 11:00:22 +0300

    Create db_actions.cs
    Commands for Npgsql

---
+++ src/DynPgsql.Lib/db_actions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynPgsql.Lib
+{
+    public class db_actions
+    {
+        public static void RunQuerry (string sql_command, string conn_config)
+        {
+            ManageConnection conn = new ManageConnection(conn_config);
+            conn.RunCommand(sql_command);
+            conn.Close();
+        }
+    }
+}
