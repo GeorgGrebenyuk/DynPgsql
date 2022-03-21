7961400003b97c9c85f89af384d85458a06edf32
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-21 10:59:17 +0300

    Update Connect.cs
    Update methods

--- src/DynPgsql/Connect.cs
+++ src/DynPgsql/Connect.cs
@@ -7,17 +7,27 @@ using System.Threading.Tasks;
 using aj = Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Runtime;
 using System.Diagnostics;
-//using DynPgsql.Lib;
+using DynPgsql.Lib;
 
 namespace DynPgsql
 {
 	public class Connect
 	{
 		private Connect() { }
+		[MultiReturn(new[] { "Name", "Author", "Source code" })]
+		public static Dictionary<string, string> PackageInfo()
+		{
+			return new Dictionary<string, string>()
+			{
+				{"Name", "DynPgsql package for AutodeskDynamo" },
+				{"Author", "GeorgGrebenyuk" },
+				{"Source code", "https://github.com/GeorgGrebenyuk/DynPgsql" }
+			};
+		}
 		public static string SetConnectSettings(string host_name, string Port, string Username, string Password, string db_name)
 		{
-			//return $"Host={host_name};Port={Port};Username={Username};Password={Password};Database={db_name};";
-			return $"host={host_name} port={Port} user={Username} password={Password} dbname={db_name}";
+			return $"Host={host_name};Port={Port};Username={Username};Password={Password};Database={db_name};";
+			//return $"host={host_name} port={Port} user={Username} password={Password} dbname={db_name}";
 		}
 		public static string SaveQuerry (string row_sql)
         {
@@ -25,34 +35,25 @@ namespace DynPgsql
 			File.WriteAllText(path, row_sql);
 			return path;
         }
-		public static string RunQuerry (string path_to_sql, string path_to_pgsql_dir_bin, string conn_config)
+		[IsVisibleInDynamoLibrary(false)]
+		public static string RunQuerry0 (string path_to_sql, string path_to_pgsql_dir_bin, string conn_config)
         {
-			string command = $"cd {path_to_pgsql_dir_bin}" + Environment.NewLine + " psql " + "\"" +
-				conn_config + "\"" + " >" + path_to_sql + Environment.NewLine;
+			string command = $"cd {path_to_pgsql_dir_bin}" + Environment.NewLine + "psql " + "\"" +
+				conn_config + "\"" + " >" + path_to_sql.Replace("\\","/") + Environment.NewLine;
 
-			ProcessStartInfo cmd_process = new ProcessStartInfo();
-			cmd_process.FileName = @"C:\Windows\System32\cmd.exe";
-			cmd_process.Arguments = command;
-			cmd_process.WindowStyle = ProcessWindowStyle.Hidden;
-			Process.Start(cmd_process);
+			Process cmd_process = new Process();
+			cmd_process.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+			cmd_process.StartInfo.Arguments = command;
+			cmd_process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			cmd_process.Start();
 			return command;
         }
+		public static void RunQuerry(string sql, string conn_config)
+		{
+			db_actions.RunQuerry(sql, conn_config);
+		}
+
 
 
 	}
-	
-	public class About
-    {
-		private About() { }
-		[MultiReturn(new[] { "Name", "Author", "Source code" })]
-		public static Dictionary<string,string> PackageInfo()
-        {
-			return new Dictionary<string, string>()
-			{
-				{"Name", "DynPgsql package for AutodeskDynamo" },
-				{"Author", "GeorgGrebenyuk" },
-				{"Source code", "https://github.com/GeorgGrebenyuk/DynPgsql" }
-			};
-        }
-    }
 }
