f7333d04f8e21d97e027a46f483e9975e961cfc0
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-21 11:00:53 +0300

    Class for sql's commands only (general)

---
+++ src/DynPgsql/Commands/Commands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynPgsql.Commands
+{
+    public class Commands
+    {
+        public static string TableCreate (DynPgsql.Table.Table table)
+        {
+            return table.create_command;
+        }
+        public static string TableDrop (string t_name)
+        {
+            return $"DROP TABLE if exists {t_name};";
+        }
+        public static string GetGeometryRepresentation(bool isCollect, DynPgsql.Geometry.Geometry geometry, int SRID = 0)
+        {
+            if (isCollect) return $"ST_GeomFromText('{geometry.geom}',{SRID})";
+            else return $"ST_GeomFromText('GEOMETRYCOLLECTION({geometry.geom})',{SRID})";
+        }
+    }
+}
